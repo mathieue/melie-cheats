76605c043e0caa51a927d7ffd57aa0014a3baed1
Mathieu Elie <mathieuelie@gmail.com>
2012-10-18 23:14:00 +0200

    install form curl

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 CHEATS_SRC=~/src/melie-cheats
+
+mkdir -p ~/src
+cd ~/src && git clone https://github.com/mathieue/melie-cheats.git
+
 mkdir -p ~/bash_completion.d
 cp -av ${CHEATS_SRC}/bash_completion.d/* ~/.bash/completion/
 cp -av ${CHEATS_SRC}/bin/* ~/bin/
